2e77d29b66964031eda8d334f133109b65397e98
kmsu44 <kmsu444@naver.com>
2022-11-14 02:34:49 +0900

    table 속도개선

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from html_table_parser import parser_functions as parser
-import urllib
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -7,10 +6,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Union
 from datetime import datetime, date
-import ssl
-from concurrent.futures import ThreadPoolExecutor
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+import asyncio
+import aiohttp
+from timeit import default_timer as dt
 import time
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+from timeit import default_timer as dt
 app = FastAPI()
 
 # 로그인
@@ -101,253 +102,239 @@ def Checklist(id,password):
 
   return result
 
-@app.get("/Table/{id}/{password}/{year}/{month}")
-def Table(id,password,year,month):
-    # month = 0~11
+@app.get("/Table/{year}/{month}")
+def Table(year,month):
     start = time.time()
-    session = requests.session()
-    # login = "https://portal.sejong.ac.kr/jsp/login/login_action.jsp"
-
-    # my={
-    #     'mainLogin': 'Y',
-    #     'rtUrl': 'blackboard.sejong.ac.kr',
-    #     'id': id,
-    #     'password': password,
-    # }
-    # header={
-    #     "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-    #     "Referer" : "https://portal.sejong.ac.kr"
-    #     }
-    # r = session.post(url = login, data=my, headers=header, timeout = 3)
-    # url = "http://library.sejong.ac.kr/sso/Login.ax"
-    # r = session.post(url,verify=False)
     url = "https://library.sejong.ac.kr/studyroom/BookingTable.axa"
-    r = session.post(url,verify=False)
-
     roomdata =[
-        {
-            'roomId': 23,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 24,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 25,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 26,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 27,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 28,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 29,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 30,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 31,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 32,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 33,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 8,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 48,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 49,
-            'year' : year,
-            'month': month,
-        },
-        {
-            'roomId': 47,
-            'year' : year,
-            'month': month,
-        }
-    ]
-
-# 테이블 정보 가져오기
-    def gettable(args):
-        a = session.post(args[0], data = args[1] , verify=False)
-        return a
-    url = "https://library.sejong.ac.kr/studyroom/BookingTable.axa"
-    list_of_urls = []
-    for room in roomdata:
-        list_of_urls.append((url,room)) 
-    a = time.time()
-    with ThreadPoolExecutor(max_workers=5) as pool:
-        response_list = list(pool.map(gettable,list_of_urls))
+            {
+                'roomId': 23,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 24,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 25,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 26,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 27,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 28,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 29,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 30,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 31,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 32,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 33,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 8,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 48,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 49,
+                'year' : 2022,
+                'month': 11,
+            },
+            {
+                'roomId': 47,
+                'year' : 2022,
+                'month': 11,
+            }
+        ]
+    data = []
+    async def get_html(room):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url,data = room, ssl = False) as response:
+                data.append(await response.text())
 
 
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(
+        asyncio.gather(
+            *(get_html(room) for room in roomdata)
+        )
+    )
     result = [
-        {
-            'roomId': 23,
-            'name': '14 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 24,
-            'name': '15 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 25,
-            'name': '16 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 26,
-            'name': '17 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 27,
-            'name': '18 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 28,
-            'name': '19 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 29,
-            'name': '20 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 30,
-            'name': '21 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 31,
-            'name': '22 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 32,
-            'name': '23 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 33,
-            'name': '24 스터디룸 (4층)',
-            'opentime': 10,
-            'closetime': 21,
-            'minuser': 3,
-            'maxuser': 8,
-        },
-        {
-            'roomId': 8,
-            'name': '교육실 (2층)',
-            'opentime': 10,
-            'closetime': 17,
-            'minuser': 2,
-            'maxuser': 25,
-        },
-        {
-            'roomId': 48,
-            'name': '대양 AI 콜라보랩 Talk Room3',
-            'opentime': 10,
-            'closetime': 16,
-            'minuser': 2,
-            'maxuser': 4,
-        },
-        {
-            'roomId': 49,
-            'name': '대양 AI 콜라보랩 라운지A',
-            'opentime': 10,
-            'closetime': 16,
-            'minuser': 2,
-            'maxuser': 4,
-        },
-        {
-            'roomId': 47,
-            'name': '대양 AI 콜라보랩 라운지A',
-            'opentime': 10,
-            'closetime': 16,
-            'minuser': 2,
-            'maxuser': 4,
-        }]
-    for idx,data in enumerate(response_list):
-        html = ' '.join(data.text.split())
-        soup = BeautifulSoup(html, "html.parser")
-        table_html = soup.find_all('table')
-        table_arry = pd.read_html(str(table_html))
-        table = table_arry[1]
-        table = table.drop(0,axis = 1)
-        a = len(table.index)
-        tmp =[]
-        for i in range(a):
-            tmp.append(table.iloc[i].to_list())
-        result[idx]["timetable"] = tmp;
+            {
+                'roomId': 23,
+                'name': '14 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 24,
+                'name': '15 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 25,
+                'name': '16 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 26,
+                'name': '17 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 27,
+                'name': '18 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 28,
+                'name': '19 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 29,
+                'name': '20 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 30,
+                'name': '21 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 31,
+                'name': '22 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 32,
+                'name': '23 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 33,
+                'name': '24 스터디룸 (4층)',
+                'opentime': 10,
+                'closetime': 21,
+                'minuser': 3,
+                'maxuser': 8,
+            },
+            {
+                'roomId': 8,
+                'name': '교육실 (2층)',
+                'opentime': 10,
+                'closetime': 17,
+                'minuser': 2,
+                'maxuser': 25,
+            },
+            {
+                'roomId': 48,
+                'name': '대양 AI 콜라보랩 Talk Room3',
+                'opentime': 10,
+                'closetime': 16,
+                'minuser': 2,
+                'maxuser': 4,
+            },
+            {
+                'roomId': 49,
+                'name': '대양 AI 콜라보랩 라운지A',
+                'opentime': 10,
+                'closetime': 16,
+                'minuser': 2,
+                'maxuser': 4,
+            },
+            {
+                'roomId': 47,
+                'name': '대양 AI 콜라보랩 라운지A',
+                'opentime': 10,
+                'closetime': 16,
+                'minuser': 2,
+                'maxuser': 4,
+            }]
+    for idx,data in enumerate(data):
+            html = ' '.join(data.split())
+            soup = BeautifulSoup(html, "html.parser")
+            table_html = soup.find_all('table')
+            table_arry = pd.read_html(str(table_html))
+            table = table_arry[1]
+            table = table.drop(0,axis = 1)
+            a = len(table.index)
+            tmp =[]
+            for i in range(a):
+                tmp.append(table.iloc[i].to_list())
+            result[idx]["timetable"] = tmp;
+
+
+
+
+
     end = time.time()
     print(end-start)
     return result
