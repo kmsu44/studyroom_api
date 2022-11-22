bf318203a183981500009179c6f08ed73376f235
kmsu44 <kmsu444@naver.com>
2022-11-23 03:11:32 +0900

    test

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def Checklist(user:User):
             room["bookingId"] = studyroom_id[idx][0]
             room["roomId"] = studyroom_id[idx][1]
             result.append(room)
-    return result
+        return result
 @app.post("/Table/")
 def Table(date : Date):
     start = time.time()
